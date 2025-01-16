8c52d7babaa15a7a0ee12d69d7419bd5b125ee4b
ori0nis <oihanegdcl@gmail.com>
2025-01-16 17:49:55 +0100

    removed wrong vercel deploy

--- proyecto-final/proyecto-final/src/components/data/fetch.js
+++ proyecto-final/proyecto-final/src/components/data/fetch.js
@@ -1,7 +1,7 @@
 import { parseDate } from "./date-parsing";
 import { renderTrainingData } from "../render/render.js";
 
-const APIURL = "https://training-dashboard-ori0nis.vercel.app/training-sessions";
+const APIURL = "https://training-dashboard-backend.vercel.app/training-sessions";
 const dataContainer = document.querySelector("#training-data");
 
 
